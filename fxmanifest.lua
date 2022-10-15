0c7240a0b4ae57d91f7b160a5511941be316bbab
Quasar <91174081+quasar-scripts@users.noreply.github.com>
2022-10-14 21:50:05 -0300

    Thanks K4MB1!

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'K4mb1 Starter Shells'
+
+this_is_a_map 'yes'
+
+files 'starter_shells_k4mb1.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'starter_shells_k4mb1.ytyp'
